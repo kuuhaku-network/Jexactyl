d978bdb221c2fc50f61ad8cd2c8db18565bf6841
kuuhaku-network <120226628+kuuhaku-network@users.noreply.github.com>
2022-12-11 18:46:29 +0900

    Update ConsoleShareContainer.tsx

--- resources/scripts/components/server/console/ConsoleShareContainer.tsx
+++ resources/scripts/components/server/console/ConsoleShareContainer.tsx
@@ -28,7 +28,7 @@ export default () => {
                 addFlash({
                     key: 'console:share',
                     type: 'success',
-                    message: 'Your server logs have been saved to the /share folder.',
+                    message: 'サーバーのログが /share フォルダに保存されました。',
                 });
             })
             .catch((error) => {
@@ -52,7 +52,7 @@ export default () => {
                 <span className={'text-gray-400'}>Offline</span>
             ) : (
                 <div className={'cursor-pointer'} onClick={submit}>
-                    Save
+                    保存
                 </div>
             )}
         </>
